52c0ee1777b8b90571ac601c13bc2b6ffe76b560
AlawatRajesh <154254493+AlawatRajesh@users.noreply.github.com>
2025-01-01 16:56:20 +0530

    Table Loading

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,65 +1,94 @@
-  <template>
+
+ <template>
   <div>
     <Alert v-if="apiMessage" :message="apiMessage" :type="alertType" :duration="1900" />
+    
+    <Alert v-if="syncingAlert" :type="type" :message="messsage" class="text-center my-2" style="height: 40px; line-height: 40px;"/>
+     
     <div v-if="apiMessage">
       <div v-for="(msg, index) in apiMessage" :key="index">
         <p>{{ msg.apiMessage }}</p>
       </div>
-      </div>
+    </div>
+
     <b-table
-      v-if="customers && customers.length > 0 "
       :items="customers"
       :fields="fields"
+      :busy="loading"
       responsive
       class="hovered table-bordered"
     >
+      <template v-slot:table-busy>
+        <div class="spinner-container">
+          <b-spinner variant="primary" label="Loading..."></b-spinner>
+        </div>
+      </template>
+
       <template #cell(view)="row">
-        <b-button   v-b-modal.modal-1 size="sm" variant="primary" @click="viewCustomer  (row.item)">
+        <b-button v-b-modal.modal-1 size="sm" variant="primary" @click="viewCustomer(row.item)">
           View
         </b-button>
       </template>
+
       <template #cell(sync)="row">
-        <b-button size="sm" variant="success" @click="syncCustomer(row.item)">
+        <b-button size="sm" variant="success" @click="syncCustomer(row.item)" :disabled="syncInProgress || row.item.syncing">
           Sync
         </b-button>
       </template>
     </b-table>
 
-    <b-alert v-if="customers.length === 0" show variant="info" class="text-center my-3">
+    <b-alert v-if="!loading && customers.length === 0" show variant="info" class="text-center my-3">
       No customer details available
     </b-alert>
   </div>
- 
 </template>
-
 <script>
 import { ref } from 'vue';
 import axios from 'axios';
-import Alert from './Alert.vue';  
+import Alert from './Alert.vue';
 
 export default {
   name: 'Table',
+  components: {
+    Alert,
+  },
   props: {
     customers: {
       type: Array,
+      required: true,
+    },
+    loading: {  
+      type: Boolean,
+      default: false,
+    },
+    message: {
+      type: String,
       required: true
     },
-  },
-  components: {
-    Alert
+    type: {
+      type: String,
+      default: 'info'
+    },
+    messsage:{
+      type: String,
+      default:true
+    }
   },
   data() {
     return {
       fields: [
-        { key: 'sequenceNumber', label: 'Sequence Number',tdClass: 'small-column',thClass: 'small-column' },
-        { key: 'createdAt', label: 'Created At' ,tdClass: 'small-column',thClass: 'small-column' },
-        { key: 'refNumber', label: 'Invoice No',tdClass: 'small-column',thClass: 'small-column'},
-        { key: 'apiResponse', label: 'Zoho Message', tdClass: 'zoho-message-column',thClass: 'zoho-message-column' },
-        { key: 'view', label: 'Receipts', tdClass: 'small-columns',thClass: 'small-columns' },
-        { key: 'sync', label: 'Sync', tdClass: 'small-columns',thClass: 'small-columns' }
+        { key: 'sequenceNumber', label: 'Sequence Number', tdClass: 'small-column', thClass: 'small-column' },
+        { key: 'createdAt', label: 'Created At', tdClass: 'small-column', thClass: 'small-column' },
+        { key: 'refNumber', label: 'Invoice No', tdClass: 'small-column', thClass: 'small-column' },
+        { key: 'apiResponse', label: 'Zoho Message', tdClass: 'zoho-message-column', thClass: 'zoho-message-column' },
+        { key: 'view', label: 'Receipts', tdClass: 'small-columns', thClass: 'small-columns' },
+        { key: 'sync', label: 'Sync', tdClass: 'small-columns', thClass: 'small-columns' }
       ],
-      apiMessage: '', 
-      alertType: '' 
+      syncingAlert: false,
+      syncInProgress: false,
+      messsage:' Syncing in progress...',
+      apiMessage: '',
+      alertType: '',
     };
   },
   methods: {
@@ -67,59 +96,65 @@ export default {
       this.$emit('view-customer', customer);
     },
     async syncCustomer(customer) {
-  this.apiMessage = '';
-  this.alertType = '';
+      if (this.syncInProgress || customer.syncing) return;
 
-  try {
-    const token = import.meta.env.VITE_API_BEARER_TOKEN;
-    if (!token) {
-      console.error('No token found in .env file');
-      return;
-    }
+      customer.syncing = true;
+      this.apiMessage = '';
+      this.alertType = '';
+      this.syncingAlert = true; 
+      this.syncInProgress = true;
 
-    const response = await axios.post(
-      `${import.meta.env.VITE_APP_API_SYNC_CUSTOMER}?referenceNumber=${customer.refNumber}&workshopId=${customer.workshopId}`,
-      {},
-      {
-        headers: {
-          'Authorization': `Bearer ${token}`
+      try {
+        const token = import.meta.env.VITE_API_BEARER_TOKEN;
+        if (!token) {
+          console.error('No token found in .env file');
+          return;
         }
-      }
-    );
 
-    console.log('Response from API:', response.data);
+        const response = await axios.post(
+          `${import.meta.env.VITE_APP_API_SYNC_CUSTOMER}?referenceNumber=${customer.refNumber}&workshopId=${customer.workshopId}`,
+          {},
+          {
+            headers: {
+              'Authorization': `Bearer ${token}`
+            }
+          }
+        );
 
-    if (response.data && response.data.data) {
-      const zohoResponse = response.data.data;
-      console.log('ZOHO_INVOICE_RESPONSE:', zohoResponse);
-      if (zohoResponse) {
-        let zohoArray = zohoResponse[0];
-        let res = zohoArray[zohoArray.length - 1];
-        this.apiMessage = res.ZOHO_INVOICE_RESPONSE.code + ":" + res.ZOHO_INVOICE_RESPONSE.message;
-        this.alertType = 'success';
+        console.log('Response from API:', response.data);
 
-       
-        this.$emit('remove-customer', customer.id); 
-      } else {
-        this.apiMessage = 'No message available in Zoho response';
+        if (response.data && response.data.data) {
+          const zohoResponse = response.data.data;
+          console.log('ZOHO_INVOICE_RESPONSE:', zohoResponse);
+          if (zohoResponse) {
+            let zohoArray = zohoResponse[0];
+            let res = zohoArray[zohoArray.length - 1];
+            this.apiMessage = res.ZOHO_INVOICE_RESPONSE.code + ":" + res.ZOHO_INVOICE_RESPONSE.message;
+            this.alertType = 'success';
+
+            this.$emit('remove-customer', customer.id);
+          } else {
+            this.apiMessage = 'No message available in Zoho response';
+            this.alertType = 'danger';
+          }
+        } else {
+          this.apiMessage = `Code: ${response.data.data}, Message: ${response.data.data}`;
+          this.alertType = 'danger';
+        }
+        
+      } catch (error) {
+        console.error('Error syncing customer:', error);
+        this.apiMessage = `Sync failed!\n\nERROR: ${error.message}`;
         this.alertType = 'danger';
+      } finally {
+        customer.syncing = false;
+        this.syncInProgress = false;
+        this.syncingAlert = false; 
       }
-    } else {
-      this.apiMessage = `Code: ${response.data.data}, Message: ${response.data.data}`;
-      this.alertType = 'danger';
     }
-  } catch (error) {
-    console.error('Error syncing customer:', error);
-    this.apiMessage = `Sync failed!\n\nERROR: ${error.message}`;
-    this.alertType = 'danger';
   }
 }
-  }
-};
-
-</script>    
-
-
+</script>
 <style>
 table tbody tr:hover {
   background-color: #e9ecef;
@@ -139,9 +174,9 @@ table tbody tr:nth-child(even) {
 
 td {
   background-color: #fff;
-  word-wrap: break-word; 
-  white-space: normal;   
-  overflow-wrap: break-word; 
+  word-wrap: break-word;
+  white-space: normal;
+  overflow-wrap: break-word;
 }
 
 table tbody {
@@ -150,19 +185,32 @@ table tbody {
   overflow-y: auto;
   scrollbar-width: thin;
 }
+
 .small-column {
-  width: 120px !important; 
+  width: 120px !important;
 }
+
 .small-columns {
-  width: 100px !important; 
+  width: 100px !important;
 }
+
 .zoho-message-column {
-  width: 300px !important; 
+  width: 300px !important;
 }
 
+.spinner-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+}
+.my-2 {
+  height: 40px !important;
+  line-height: 40px !important;
+}
+</style> 
 
 
 
 
-</style>
 
